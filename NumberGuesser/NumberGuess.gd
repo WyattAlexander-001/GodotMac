023e9a2ab7d1014a0b4428deecc65067d8320437
WyattAlexander-001 <wyattbushman117@gmail.com>
2023-11-02 22:05:55 -0400

    finished game

--- NumberGuesser/NumberGuess.gd
+++ NumberGuesser/NumberGuess.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var message = $Message
 @onready var victory = $Victory
+@onready var yes = $Yes
 
 var guess
 var count = 1
@@ -24,10 +25,7 @@ func _process(delta):
 		_try_guess("down")
 		count+=1
 	elif Input.is_action_just_pressed("space"):
-		if game_end == true:
-			_restart_game()
-		else:
-			_end_game()
+		_on_yes_pressed()
 
 func _try_guess(type):
 	if type == "up":
@@ -41,9 +39,28 @@ func _try_guess(type):
 func _end_game():
 	game_end = true
 	print("Yes! Over at attempts: " + str(count))
-	victory.text = "Yes! Over at attempts: " + str(count) + " SPACE to restart"
-	
+	victory.text = "Yes! Over at attempts: " + str(count) + " SPACE/Button to restart"
+	yes.text = "Restart???"
 
 func _restart_game():
 	get_tree().reload_current_scene()
 	
+
+# Signal Funcs
+func _on_greater_pressed():
+	print("GREATER")
+	count+=1 
+	_try_guess("up")
+
+
+func _on_lesser_pressed():
+	print("lesser")
+	count+=1
+	_try_guess("down")
+
+func _on_yes_pressed():
+	print("gottem!")
+	if game_end == true:
+		_restart_game()
+	else:
+		_end_game()
